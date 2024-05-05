2ea73aee81c734e54ffa506f408face065f9a1ed
Akshat <122510638+Axshat0@users.noreply.github.com>
2024-05-05 18:47:44 +0530

    js

---
+++ JavaScript/data.js
@@ -0,0 +1,25 @@
+// Data type
+// Number
+let number = 123
+
+// String
+let name = "Spongebob"
+
+// Boolean
+let pi = 3.14
+
+// Null
+let age = null
+
+//Undefined
+let stars = undefined
+
+// Object
+const student = {
+    name : "Sasuke",
+    age : 27,
+    cgpa : 8.9,
+    isPass : true
+};
+console.log(student);
+console.log(student.name)
