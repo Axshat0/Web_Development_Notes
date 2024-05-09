1620d49121b9fdab4fc1daccdb5ef9e4d987fc4e
Akshat <122510638+Axshat0@users.noreply.github.com>
2024-05-09 21:07:37 +0530

    dom

--- JavaScript/dom.js
+++ JavaScript/dom.js
@@ -1,1 +1,47 @@
-// coming soon
+// DOM
+// When a web page is loaded, the browser creates a Document Object Model (DOM) of the page
+
+// DOM Manipulation
+
+// Selecting with id
+let myId = document.getElementById("myId");
+console.log(myId);
+console.dir(myId);
+
+// Selecting with class
+let myClass = document.getElementsByClassName("myClass");
+console.log(myClass);
+console.dir(myClass);
+
+// Selecting with tag
+let tag = document.getElementsByTagName("h1");
+console.log(tag);
+console.dir(tag);
+
+// Query Selector
+console.dir(document.querySelector(".myClass"));
+console.dir(document.querySelector("#myId"));
+console.dir(document.querySelector("h1"));
+
+// tagName
+let btn = document.querySelector(".btn");
+console.log(btn.tagName);
+
+// innerText
+console.log(myId.innerText);
+
+// innerHTML
+let pss = document.querySelector(".pss");
+console.log(pss.innerHTML);
+
+// textContent
+let div = document.querySelector(".class-div");
+console.log(div.textContent);
+
+// getAttribute
+console.log(div.getAttribute("class"));
+console.log(div.getAttribute("id"));
+
+// setAttribute
+let oldClass = document.querySelector("div");
+console.log(oldClass.setAttribute("oldClass", "newClass")); 
